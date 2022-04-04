5c6416c35431030eba1f2926124beda6725b39f2
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-04 21:00:24 +0200

    implement Customers controller methods that delegate CRUD operations to Customers service

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,32 +1,45 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CustomerDto } from './customer.dto';
 import { CustomersService } from './customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
 
-  @Post()
-  public create() {
-    return 'This should create customer with specified data';
+  @Post('/')
+  public create(@Body() customer: CustomerDto): CustomerDto {
+    return this.customersService.create(customer);
   }
 
-  @Get()
-  public findAll() {
-    return 'This should be an array with all customers';
+  @Get('/')
+  public findAll(): CustomerDto[] {
+    return this.customersService.findAll();
   }
 
   @Get(':id')
-  public findeOne() {
-    return 'This should be a customer with specified id';
+  public findeOne(@Param('id', ParseIntPipe) id: number) {
+    return this.customersService.findOne(id);
   }
 
   @Put(':id')
-  public update() {
-    return 'This should update specified customer with specified data';
+  public update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() customer: CustomerDto,
+  ) {
+    return this.customersService.update(id, customer);
   }
 
   @Delete(':id')
-  public remove() {
-    return 'This should delete customer with specified id';
+  public remove(@Param('id', ParseIntPipe) id: number): void {
+    return this.customersService.remove(id);
   }
 }
