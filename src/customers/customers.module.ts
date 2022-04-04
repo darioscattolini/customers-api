5f952ae444552a24d97a835db2cf4f86ff6d6910
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-04 18:27:15 +0200

    add Customers controller boilerplate

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CustomersController } from './customers.controller';
 
-@Module({})
+@Module({
+  controllers: [CustomersController],
+})
 export class CustomersModule {}
