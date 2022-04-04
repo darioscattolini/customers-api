53c0d587becea5dce032b08a5e0d02fac79984f9
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-04 19:15:08 +0200

    add routes in Customers controller

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  @Post()
+  public create() {
+    return 'This should create customer with specified data';
+  }
+
+  @Get()
+  public findAll() {
+    return 'This should be an array with all customers';
+  }
+
+  @Get(':id')
+  public findeOne() {
+    return 'This should be a customer with specified id';
+  }
+
+  @Put(':id')
+  public update() {
+    return 'This should update specified customer with specified data';
+  }
+
+  @Delete(':id')
+  public remove() {
+    return 'This should delete customer with specified id';
+  }
+}
