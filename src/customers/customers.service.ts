417d2b301103da5e8890ec8aa9c2e14d884bf398
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-04 20:39:12 +0200

    implement Customers service methods with persistence in internal array

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { CustomerDto } from './customer.dto';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  private customers: CustomerDto[] = [];
+
+  public create(customer: CustomerDto): CustomerDto {
+    this.customers.push(customer);
+    return customer;
+  }
+
+  public findAll(): CustomerDto[] {
+    return this.customers.slice();
+  }
+
+  public findOne(id: number): CustomerDto {
+    return this.customers.find((customer) => customer.id === id);
+  }
+
+  public remove(id: number): void {
+    const index = this.customers.findIndex((customer) => customer.id === id);
+    this.customers.splice(index, 1);
+  }
+
+  public update(id: number, customer: CustomerDto): CustomerDto {
+    const index = this.customers.findIndex((customer) => customer.id === id);
+
+    customer = {
+      ...customer,
+      id,
+    };
+
+    this.customers[index] = customer;
+
+    return customer;
+  }
+}
