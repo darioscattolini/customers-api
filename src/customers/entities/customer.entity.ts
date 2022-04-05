aee9cadc713d5e1b6c7fed1740365f1261945519
Darío Scattolini <dario.scattolini@gmail.com>
2022-04-05 02:48:33 +0200

    add Customer entity with validation constraints, update Customer dto validation constraints

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,36 +1,56 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import {
   IsDateString,
+  IsDefined,
   IsEmail,
   IsInt,
   IsNotEmpty,
-  IsOptional,
   IsPositive,
   IsString,
   MaxLength,
 } from 'class-validator';
 
-export class CustomerDto {
-  @IsOptional()
+@Entity()
+export class Customer {
+  @PrimaryGeneratedColumn()
+  @IsDefined()
   @IsInt()
   @IsPositive()
   public id: number;
 
+  @Column({
+    length: 50,
+    nullable: false,
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(50)
   public name: string;
 
+  @Column({
+    length: 50,
+    nullable: false,
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(50)
   public surname: string;
 
+  @Column({
+    length: 254,
+    nullable: false,
+    unique: true,
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(254)
   @IsEmail()
   public email: string;
 
+  @Column({
+    length: 10,
+    nullable: false,
+  })
   @IsString()
   @IsNotEmpty()
   @IsDateString()
